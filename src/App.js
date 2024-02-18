78d43c61f90c543eb26e17dba5dc4a5d0902e201
ifumikuah <detruxified@proton.me>
2024-02-18 17:33:14 +0700

    eps-ii

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import First from './components/first';
+import Header from './Header';
 
 function App() {
   return (
-    <div>
+    <>
+      <Header/>
       <First hewan="ikan"/>
       <First hewan="kuda"/>
-    </div>
+    </>
   );
 }
 
