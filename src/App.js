0213823b33ef4d16f4e6183e1ef9f23319b6369a
ifumikuah <detruxified@proton.me>
2024-02-19 21:16:05 +0700

    eps-4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
-import Card from './Card';
+// import Card from './Card';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function updateCounter() {
+    setCounter(counter + 1)
+  }
+
   return (
     <>
-      <Card/>
-      <Card/>
+      <h2>You clicked the button {counter} times</h2>
+      <button onClick={()=> updateCounter()} >Click to Counter</button>
     </>
   );
 }
