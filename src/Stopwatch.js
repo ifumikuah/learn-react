162cfeb065f03ad38b0eab68d91a0f0f86892546
ifumikuah <detruxified@proton.me>
2024-03-03 23:35:41 +0700

    actual stopwatch

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,52 +1,74 @@
 import React, { useState, useEffect, useRef } from "react";
 
 export default function Stopwatch() {
-  const [active, setActive] = useState(false);
-  const [elapsed, setElapsed] = useState(0);
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState([])
+  const [isActive, setIsActive] = useState(false);
   const intervalId = useRef(null);
-  const delta = useRef(0);
-
-  const start = () => {
-    setActive(true)
-    delta.current = Date.now() - elapsed
-  }
-
-  const pause = () => {
-    setActive(false)
-  }
-  
-  const reset = () => {
-    setElapsed(0)
-    setActive(false)
-  }
 
   useEffect(() => {
-    if (active) {
+    if (isActive) {
       intervalId.current = setInterval(() => {
-        setElapsed(Date.now() - delta.current)
-      }, 10);
+        setTime((t) => t + 1);
+      }, 100);
     }
+
     return () => {
-      clearInterval(intervalId.current)
+      clearInterval(intervalId.current);
+    };
+  }, [isActive]);
+
+  const start = () => {
+    setIsActive(true);
+  };
+
+  const pause = () => {
+    setIsActive(false);
+  };
+
+  const reset = () => {
+    setIsActive(false);
+    setTime(0);
+    setLaps([])
+  };
+
+  const formatter = (number) => {
+    const second = Math.floor((number / 10) % 60);
+    const minute = Math.floor((number / 10 / 60) % 60);
+    const hour = Math.floor(number / 10 / 3600);
+
+    return {
+      hour: hour.toString().padStart(2, "0"),
+      minute: minute.toString().padStart(2, "0"),
+      second: second.toString().padStart(2, "0"),
+    };
+  };
+
+  const lap = () => {
+    if (time) {
+      const capturedLap = `${formatter(time).hour}:${formatter(time).minute}:${formatter(time).second}.${time % 10}`
+      setLaps(l => [...l, capturedLap])
     }
-  }, [active])
-
-  const displayStopwatch = () => {
-    const milisecond = Math.floor((elapsed % 1000) / 10).toString().padStart(2, "0")
-    const second = Math.floor(elapsed / 1000 % 60).toString().padStart(2, "0")
-    const minute = Math.floor(elapsed / (1000 * 60) % 60).toString().padStart(2, "0")
-
-    return `${minute}:${second}:${milisecond}`
   }
 
   return (
     <div className="Stopwatch">
-      <h2>{ displayStopwatch() }</h2>
-      <div className="container flex">
-        <button className="button start" onClick={start}>Start</button>
-        <button className="button pause" onClick={pause}>Pause</button>
-        <button className="button reset" onClick={reset}>Reset</button>
+      <h2 className="timer">
+        {formatter(time).hour}:{formatter(time).minute}:{formatter(time).second}
+        <span className="milisecond">.{time % 10}</span>
+      </h2>
+      <div className="buttons">
+        {isActive ?
+          <button className="button" onClick={pause}> Pause </button>
+          :
+          <button className="button" onClick={start}> Start </button>
+        }
+        <button className="button" onClick={lap}> Lap </button>
+        <button className="button" onClick={reset}> Reset </button>
       </div>
+      <ol className="laps-list">
+        {laps.map((e, i) => <li key={i}> { i+1 }. { e }</li>)}
+      </ol>
     </div>
   );
 }
